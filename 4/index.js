ccc63624a09fb1a6f7404152a82e5e7d645e1d9d
Xavier <xavier3435@icloud.com>
2024-09-17 12:12:24 -0400

    functions added and edited

--- 4/index.js
+++ 4/index.js
@@ -2,6 +2,17 @@ let secondsElapsed = 0;
 let interval = null;
 const time = document.getElementById("time")
 
+function padStart(value) {
+    return String(value).padStart(2, "0")
+}
+
+function setTime() {
+    const minutes = Math.floor(secondElapsed / 60)
+    const seconds = secondsElapsed % 60
+    time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}`;
+}
+
+
 function setTimeout() {
     time.innerHTML = secondsElapsed;
 }
@@ -12,9 +23,16 @@ setTimeout()
 }
 
 function startClock() {
+  if (interval) resetClock()
     interval = setInterval(timer, 1000)
 }
 
-function stopClock() {}
+function stopClock() {
+    clearInterval(interval)
+}
 
-function resetClock() {}
+
+function resetClock() {
+secondsElapsed = 0
+setTime()
+}
