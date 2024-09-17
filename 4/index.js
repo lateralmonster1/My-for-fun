bcdf40fd8edcb0092d3e7b1855611069eaff75d3
Xavier <xavier3435@icloud.com>
2024-09-17 11:34:26 -0400

    fixed function

--- 4/index.js
+++ 4/index.js
@@ -2,9 +2,13 @@ let secondsElapsed = 0;
 let interval = null;
 const time = document.getElementById("time")
 
+function setTimeout() {
+    time.innerHTML = secondsElapsed;
+}
 
 function timer() {
 secondsElapsed++; 
+setTimeout()
 }
 
 function startClock() {
